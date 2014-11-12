35e2f3c497ea5d783e4055549f37641eb015ca24
David Bang <davidbang2012@gmail.com>
2014-11-11 20:33:17 -0500

    working on app and templates

--- app.py
+++ app.py
@@ -14,13 +14,13 @@ def index():
 @app.route("/weather")
 @app.route("/weather/<loc>")
 def t(loc):
-    if (loc == null or loc == ""):
+    if (loc == ""):
         loc = "New York"
     url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22" + loc + "%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
     request = urllib2.urlopen(url)
     result = request.read()
     d = json.loads(result)
-    if (d['query']['results'] == null):
+    if (d['query']['results'] == "null"):
         flash ("Invalid Location")
         return redirect ("/")
     d1 = d['query']['results']['channel']
@@ -28,7 +28,7 @@ def t(loc):
     title =  d1['description']
     location = d1['item']['title'] + " located at lattitude:" + d1 ['item']['lat'] + " longitude:" + d1 ['item']['long']
     date = d1['item']['condition']['date']
-    return render_template ("weather.html")
+    return render_template ("weather.html", title = title, location = location)
             
     
 if __name__=="__main__":
